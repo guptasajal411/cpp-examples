b04c122e348d525626b447c4ead116a612a65124
Sajal Gupta <70312106+guptasajal411@users.noreply.github.com>
2020-11-09 17:03:30 +0530

    Add files via upload

---
+++ maxTillIArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int findMaxInArray(int array[], int n){
+    int tempMaxForArray = array[0];
+    for (int i = 0; i <= n; i++){
+        if (array[i] > tempMaxForArray){
+            tempMaxForArray = array[i];
+        }
+    }
+    int maxInArray = tempMaxForArray;
+    return maxInArray;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++){
+        cin >> array[i];
+    }
+    int maxUptoArray[n];
+    for (int i = 0; i < n; i++){
+        cout << findMaxInArray(array, i) << " ";
+    }
+    return 0;
+}
---
+++ rough110011.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    cout<<arr[2];
+    cout<<arr[-1];
+
+	return 0;   
+}
